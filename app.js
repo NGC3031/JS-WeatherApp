e63b190381a7255c06402cba2653201278f8cdf7
NGC3031 <scott.d@ieee.org>
2019-03-28 17:40:20 +1100

    updated

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription=document.querySelector(".temperature-description");
+    let temperatureDegree=document.querySelector(".temperature-degree");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            long=position.coords.latitude;
-            lat=position.coords.longitude;
+            lat=position.coords.latitude;
+            long=position.coords.longitude;
 
-            //console.log(position);
-            //const proxy="https://cors-anywhere.herokuapp.com/";
-            const proxy="https://crossorigin.me/";
-            const api=`${proxy}https://api.darksky.net/forecast/2bf53a829d6d6c1febae836ee93774fe/${lat},${long}`;
+            const proxy='https://cors-anywhere.herokuapp.com/';
+            const api =`${proxy}https://api.darksky.net/forecast/2bf53a829d6d6c1febae836ee93774fe/${lat},${long}`;
             console.log(api);
+            //console.log(api2);
             console.log('hey');
             fetch(api)
             .then(response=>{
-                return response.json();
-               // console.log('hi');
+               return response.json();
+                console.log('hi');
             })
             .then(data=>{
                 console.log(data);
-            })
+                const {temperature,summary}=data.currently;
+                temperatureDegree.textContent=temperature;
+                temperatureDescription.textContent=summary;
+  })
         
         });
     } else {
